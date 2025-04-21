de1de41fe329902d7a35d3a0c4c1adcc4d02623d
webdevryann <ryan@webdevryan.com>
2025-04-21 23:14:27 +0100

    started the skeleton of the account page

---
+++ app/_components/auth/AccountNavigation.js
@@ -0,0 +1,79 @@
+"use client";
+
+import { logout } from "@/app/_lib/auth-service";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
+const links = [
+  { href: "/account/guides", label: "My Guides" },
+  { href: "/account/orders", label: "Orders" },
+  { href: "/account/saved-blog-posts", label: "Saved Blog Posts" },
+  { href: "/account/settings", label: "Settings" },
+];
+
+function AccountNavigation() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await logout();
+    router.push("/");
+  };
+
+  return (
+    <>
+      <div className="grid grid-cols-2 gap-15 md:hidden">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={`text-center pointer-cursor h-full flex justify-center items-center text-sm p-2 transition-all duration-200 font-medium bg-white shadow-sm rounded-md  ${
+              pathname === link.href
+                ? "text-accent-blue font-semibold"
+                : "text-off-black hover:scale-[97%]"
+            }`}
+          >
+            {link.label}
+          </Link>
+        ))}
+
+        <button
+          onClick={handleLogout}
+          className="cursor-pointer text-sm p-2  bg-white shadow-sm text-red-500 hover:bg-red-200 
+          hover:scale-[97%] rounded-md transition-all duration-200 "
+        >
+          Log out
+        </button>
+      </div>
+
+      <aside className="hidden md:block">
+        <nav className="flex flex-col justify-between gap-30 ">
+          {links.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={`text-sm transition font-medium ${
+                pathname === link.href
+                  ? "text-accent-blue font-semibold"
+                  : "text-off-black hover:text-accent-blue"
+              }`}
+            >
+              {link.label}
+            </Link>
+          ))}
+
+          <div>
+            <button
+              onClick={handleLogout}
+              className="text-sm w-fit text-red-500 hover:underline "
+            >
+              Log out
+            </button>
+          </div>
+        </nav>
+      </aside>
+    </>
+  );
+}
+
+export default AccountNavigation;
--- app/account/layout.js
+++ app/account/layout.js
@@ -1,49 +1,12 @@
-"use client";
-
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { logout } from "@/app/_lib/auth-service";
 import ProtectedRoute from "@/app/_components/auth/ProtectedRoute";
-
-const links = [
-  { href: "/account", label: "Overview" },
-  { href: "/account/guides", label: "My Guides" },
-  { href: "/account/orders", label: "Orders" },
-  { href: "/account/settings", label: "Settings" },
-];
+import AccountNavigation from "../_components/auth/AccountNavigation";
 
 export default function AccountLayout({ children }) {
-  const pathname = usePathname();
-
   return (
     <ProtectedRoute>
-      <section className="section-style top-page-spacing width-size flex flex-col lg:grid lg:grid-cols-[220px_1fr] gap-30 lg:gap-60">
+      <section className="section-style top-page-spacing width-size flex flex-col md:grid md:grid-cols-[220px_1fr] gap-30 lg:gap-60">
         {/* Sidebar or Top Nav */}
-        <aside className="border-b lg:border-b-0 lg:border-r pb-15  lg:pr-30">
-          <nav className="flex flex-row lg:flex-col justify-between flex-wrap ">
-            {links.map((link) => (
-              <Link
-                key={link.href}
-                href={link.href}
-                className={`text-sm px-2 transition font-medium ${
-                  pathname === link.href
-                    ? "text-accent-blue font-semibold"
-                    : "text-neutral-600 hover:text-accent-blue"
-                }`}
-              >
-                {link.label}
-              </Link>
-            ))}
-
-            <button
-              onClick={logout}
-              className="text-sm w-fit text-red-500 hover:underline "
-            >
-              Log out
-            </button>
-          </nav>
-        </aside>
-
+        <AccountNavigation />
         {/* Main Content */}
         <div className="min-h-[400px]">{children}</div>
       </section>
