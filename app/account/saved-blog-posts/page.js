0d051c23ece84d3828fa868f88990299822fe55d
webdevryann <ryan@webdevryan.com>
2025-05-31 16:16:49 +0300

    fixing typo in saved blog posts

--- app/account/saved-blog-posts/page.js
+++ app/account/saved-blog-posts/page.js
@@ -3,7 +3,9 @@ export const metadata = {
 };
 
 function page() {
-  return <div className="text-sm">You haven't saved any blog posts yet</div>;
+  return (
+    <div className="text-sm">You haven&apos;t saved any blog posts yet</div>
+  );
 }
 
 export default page;
